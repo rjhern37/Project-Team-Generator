e86b8b9ee22b076b413524a91016f9e01bb1ae33
Ray H <rjhern37@neiu.edu>
2020-04-13 21:04:39 -0500

    Uploading some beginngig files

---
+++ Homework-10/Develop/index.js
@@ -0,0 +1,60 @@
+const inquirer = require("inquirer"); 
+
+const fs = require("fs");
+
+
+//Make a set of questions for the user to fill out to populate the file/page
+// const questions = [
+//     {
+
+//         type: "input", 
+//         name: "numberOfUsers",
+//         message: "How many employees are on your team?"
+
+//     },
+
+//     {
+
+//         type: "input", 
+//         name: "teamLeader",
+//         message: "Who is the project manager?"
+
+//     },
+
+//     {
+
+//         type: "input", 
+//         name: "numberOfUsers",
+//         message: "How many employees are on your team?"
+
+//     },
+
+//     {
+
+//         type: "input", 
+//         name: "numberOfUsers",
+//         message: "How many employees are on your team?"
+
+//     },
+
+   
+
+// ]
+
+
+class emp
+
+//Make a class for employees (also use a "default" like Nick showed us)
+class employees {
+
+};
+
+function start(){
+    inquirer.prompt(questions).then(answers) => {
+
+
+
+    }
+
+
+}
